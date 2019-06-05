80ff8c60f9531118e6587274eb406926b7e5b943
berta <berta.vincze@gmail.com>
2019-06-05 23:09:31 +0200

    Add boolean isLiked field to AlbumDto

--- src/main/java/com/auditorium/dto/AlbumDto.java
+++ src/main/java/com/auditorium/dto/AlbumDto.java
@@ -10,11 +10,13 @@ public final class AlbumDto {
     private final String artist;
     private final Album album;
     private final List<Track> tracks;
+    private boolean isLiked;
 
     public AlbumDto(String artist, Album album, List<Track> tracks) {
         this.artist = artist;
         this.album = album;
         this.tracks = tracks;
+        this.isLiked = false;
     }
 
     public String getArtist() {
@@ -27,5 +29,13 @@ public final class AlbumDto {
 
     public List<Track> getTracks() {
         return tracks;
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
     }
 }
